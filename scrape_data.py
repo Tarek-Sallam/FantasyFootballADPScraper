5b6310767298ff778fe9e96a92bb46b0eae4ee2a
Tarek Sallam <tarek@sallam.ca>
2024-07-09 19:31:15 -0400

    removed projected points collection, and fixed position grabbing of ADP data

--- scrape_data.py
+++ scrape_data.py
@@ -1,8 +1,5 @@
-from fantasy_pros_scrape import scrape_adp_data, scrape_proj_data
+from fantasy_pros_scrape import scrape_adp_data
 import os
 
-path1 = os.path.join(os.getcwd(), 'raw_data', 'adp')
-path2 = os.path.join(os.getcwd(), 'raw_data', 'total_pts')
-
+path1 = os.path.join(os.getcwd(), 'raw_data')
 scrape_adp_data(path1)
-scrape_proj_data(path2)
