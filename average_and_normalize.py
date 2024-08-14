bdf007794740ca3f71df336af250b44bbc1b82fd
Tarek Sallam <tarek@sallam.ca>
2024-08-14 15:51:24 +0300

    changed line

--- average_and_normalize.py
+++ average_and_normalize.py
@@ -8,10 +8,11 @@ path = os.path.join(os.getcwd(), 'raw_data');
 for i in range(9):
     raw_data.append(pd.read_csv(os.path.join(path, 'adp_raw_' + str(2015+i) + '.csv')))
 
-all_data = pd.concat(raw_data).reset_index(drop=True).sort_values('adp')
+all_data = pd.concat(raw_data).sort_values('adp').reset_index(drop=True)
 adp = all_data['adp'].to_numpy()
 adp = (adp - np.min(adp)) / (np.max(adp) - np.min(adp))
 normalized_data = pd.DataFrame({'position': all_data['position'], 'adp': adp})
+
 adp_by_pos = {'QB': normalized_data[normalized_data['position'] == 'QB'].reset_index(drop=True)['adp'].to_numpy(), 
               'RB': normalized_data[normalized_data['position'] == 'RB'].reset_index(drop=True)['adp'].to_numpy(), 
               'WR': normalized_data[normalized_data['position'] == 'WR'].reset_index(drop=True)['adp'].to_numpy(), 
