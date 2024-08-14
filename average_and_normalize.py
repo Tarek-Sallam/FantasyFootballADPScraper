34f3878cb254ebb4e6ff4fd5665deb86f30a5eb9
Tarek Sallam <tarek@sallam.ca>
2024-08-14 14:08:58 +0300

    changed normalization by joining yearly data together and performing min-max

--- average_and_normalize.py
+++ average_and_normalize.py
@@ -8,22 +8,16 @@ path = os.path.join(os.getcwd(), 'raw_data');
 for i in range(9):
     raw_data.append(pd.read_csv(os.path.join(path, 'adp_raw_' + str(2015+i) + '.csv')))
 
-## concatenate all the year data
-all_adps = pd.concat(raw_data).sort_values('adp')
+all_data = pd.concat(raw_data).reset_index(drop=True).sort_values('adp')
+adp = all_data['adp'].to_numpy()
+adp = (adp - np.min(adp)) / (np.max(adp) - np.min(adp))
+normalized_data = pd.DataFrame({'position': all_data['position'], 'adp': adp})
+adp_by_pos = {'QB': normalized_data[normalized_data['position'] == 'QB'].reset_index(drop=True)['adp'].to_numpy(), 
+              'RB': normalized_data[normalized_data['position'] == 'RB'].reset_index(drop=True)['adp'].to_numpy(), 
+              'WR': normalized_data[normalized_data['position'] == 'WR'].reset_index(drop=True)['adp'].to_numpy(), 
+              'TE': normalized_data[normalized_data['position'] == 'TE'].reset_index(drop=True)['adp'].to_numpy(),
+              'K': normalized_data[normalized_data['position'] == 'K'].reset_index(drop=True)['adp'].to_numpy(),
+              'DEF': normalized_data[normalized_data['position'] == 'DST'].reset_index(drop=True)['adp'].to_numpy()
+}
 
-## seperate all the data by each position
-adp_by_pos = {'QB': all_adps[all_adps['position'] == 'QB'].reset_index(drop=True), 
-              'RB': all_adps[all_adps['position'] == 'RB'].reset_index(drop=True), 
-              'WR': all_adps[all_adps['position'] == 'WR'].reset_index(drop=True), 
-              'TE': all_adps[all_adps['position'] == 'TE'].reset_index(drop=True),
-              'K': all_adps[all_adps['position'] == 'K'].reset_index(drop=True),
-              'DEF': all_adps[all_adps['position'] == 'DST'].reset_index(drop=True)}
-
-all_adp = {}
-normalized_adp = {}
-
-## drop the positon column, convert to np array and normalize each position
-for pos, value in adp_by_pos.items():
-    all_adp[pos] = value['adp'].to_numpy()
-
-np.save(os.path.join(os.getcwd(), 'cleaned_data', 'adp_data.npy'), all_adp, allow_pickle=True)
+np.save(os.path.join(os.getcwd(), 'cleaned_data', 'adp_data.npy'), adp_by_pos, allow_pickle=True)
